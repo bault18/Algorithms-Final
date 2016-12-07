efe9fbb602539ff2ab82e6f331933b1749ba964b
Brian Ault <bault18@my.whitworth.edu>
2016-12-07 11:34:27 -0800

    Did some cleanup and planning

--- Algorithms Final/Main.cpp
+++ Algorithms Final/Main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <deque>
+#include <queue>
 
 using namespace std;
 
@@ -12,6 +12,62 @@ critical node where first is starting ending point
 
 
 */
+
+struct visiting
+{
+	int node;
+	double value;
+};
+
+
+//Find lowest bound of whole problem
+int get_lowest_bound_start(vector<vector<double>> total_map, vector<int> critical_nodes)
+{
+	double lowest_bound = 0;
+	//get two minimal paths from the current node and add to lowest bound
+	//expand next critical node and do the same
+	for (int i = 0; i < critical_nodes.size(); i++)
+	{
+		vector<doubles> paths;
+		for (int j = 0; j < critical_nodes.size(); j++)
+		{
+			//if possible path and not leading to itself
+			if (total_map[critical_nodes[i] - 1][critical_nodes[j] - 1] != INFINITY && j != i)
+				paths.push_back(total_map[critical_nodes[i] - 1][critical_nodes[j] - 1]);
+		}
+
+		sort(paths.begin(), paths.begin() + paths.size()); //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ maybe size() - 1
+		lowest_bound += paths[0] + paths[1];
+	}
+
+	//ceiling of lowest bound divided by two
+	lowest_bound /= 2;
+	return int(lowest_bound + 0.5);
+}
+
+//Find lowest bound given that you came from certain nodes
+double get_lowest_bound(vector<vector<double>> total_map, vector<int> critical_nodes, vector<int> path)
+{
+	//calculate lowest bound up until the end of the path
+
+	//calculate lowest bound for all points that are not in the path
+}
+
+//returns vector of critical nodes visitable by current node
+vector<int> get_children(vector<vector<double>> total_map, vector<int> critical_nodes, int current_node)
+{
+	vector<int> children;
+	for (int i = 0; i < critical_nodes.size(); i++)
+	{
+		//note nodes are one indexed
+		//If the node in the map is visitable and not the current node
+		//add to children
+		if (total_map[current_node - 1][critical_nodes[i] - 1] != INFINITY && total_map[current_node - 1][critical_nodes[i] - 1] != 0)
+			children.push_back(i);
+	}
+	
+	return children;
+}
 
 int main()
 {
@@ -55,7 +111,10 @@ int main()
 	}
 	
 	//Get critical nodes
-	deque<int> critical_nodes;
+	vector<int> critical_nodes;
+	vector<int> path;
+	vector<int> traversed;
+
 	for (int i = 0; i < num_nodes; i++)
 	{
 		int node;
@@ -64,5 +123,40 @@ int main()
 	}
 
 	//Branch and Bound for TSP
+	/*
+	while path hasn't been found
+		if first round
+			calculate lower bound
+			get all children
+				calculate their lower bound
+				expand lowest bound
+		if != first round
+			should have lower bound from last round
+			get all children
+				calulate their lower bound
+				expand lowest bound
+	*/
+	bool first_round = true;
+	while (true)
+	{
+		if (first_round == true)
+		{
+			first_round = false;
 
+			//calculate lowest bound
+			int lowest = get_lowest_bound_start(total_map, critical_nodes);
+
+			//Get children first node
+
+			//calculate lowest bound assuming you went to this child
+
+			//Keep track of which child has lowest bound and expand that one into the path
+			
+
+		}
+		else
+		{
+
+		}
+	}
 }
